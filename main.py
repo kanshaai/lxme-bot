8906b3e7df6511e39ba2741bbf54971de66e9a7a
souravvmishra <souravvmishra@gmail.com>
2024-08-14 20:30:48 +0530

    indifi

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 os.environ["SERPER_API_KEY"] = os.getenv("SERPER_API_KEY")
 
 # Company-specific details
-COMPANY_NAME = "jupiter Money"
-COMPANY_DOMAIN = "jupiter.money/"
+COMPANY_NAME = "Indifi"
+COMPANY_DOMAIN = "indifi.com"
 COMPANY_ROLE = f'{COMPANY_NAME} Information Specialist'
 COMPANY_GOAL = f'Provide accurate and detailed information about {COMPANY_NAME} products, services, and solutions available on {COMPANY_DOMAIN}'
 COMPANY_BACKSTORY = (
     f'You are a knowledgeable specialist in {COMPANY_NAME}\'s offerings. '
     f'You provide detailed information about their products, services, '
-    f'and solutions available on lxme.in, including any innovations and key features.'
+    f'and solutions available on {COMPANY_DOMAIN}, including any innovations and key features.'
 )
 
 
