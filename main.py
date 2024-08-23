5e7ba6ee356c51950b9fb2dba24cebd209cc98d6
pratham0039 <2021uee0150@iitjammu.ac.in>
2024-08-23 18:17:12 +0530

    Added bot UOB

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 os.environ["SERPER_API_KEY"] = os.getenv("SERPER_API_KEY")
 
 # Company-specific details
-COMPANY_NAME = "jupiter Money"
-COMPANY_DOMAIN = "jupiter.money/"
+COMPANY_NAME = "UOB Singapore"
+COMPANY_DOMAIN = "uobgroup.com/"
 COMPANY_ROLE = f'{COMPANY_NAME} Information Specialist'
 COMPANY_GOAL = f'Provide accurate and detailed information about {COMPANY_NAME} products, services, and solutions available on {COMPANY_DOMAIN}'
 COMPANY_BACKSTORY = (
