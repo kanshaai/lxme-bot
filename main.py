c630301bfd1a00f31ff1b9ff9d2ba076432b221f
pratham0039 <2021uee0150@iitjammu.ac.in>
2024-08-25 12:16:50 +0530

    title change

--- main.py
+++ main.py
@@ -170,7 +170,7 @@ st.markdown(custom_css, unsafe_allow_html=True)
 st.markdown("""
             
     <h1 style="color:#00305a;">
-        <span style="color:#f26624;">ICICI</span> Customer Support
+        <span style="color:#f26624;">ICICI Health Insurance</span> Customer Support
     </h1>
 """, unsafe_allow_html=True)
 st.write("<style>div.block-container{padding-top:2rem;}</style>", unsafe_allow_html=True)
