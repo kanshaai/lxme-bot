f4d0f1a4b3519146b7bad60e4b1a063ad2916138
pratham0039 <2021uee0150@iitjammu.ac.in>
2024-08-31 14:13:58 +0530

    made lxme v0.4

--- main.py
+++ main.py
@@ -19,14 +19,14 @@ os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 os.environ["SERPER_API_KEY"] = os.getenv("SERPER_API_KEY")
 
 # Company-specific details
-COMPANY_NAME = "Axi"
-COMPANY_DOMAIN = "axi.com/"
+COMPANY_NAME = "Lxme"
+COMPANY_DOMAIN = "lxme.in"
 COMPANY_ROLE = f'{COMPANY_NAME} Information Specialist'
-COMPANY_GOAL = f'Provide accurate and detailed information about {COMPANY_NAME} products, services, and solutions available on axi.com.'
+COMPANY_GOAL = f'Provide accurate and detailed information about {COMPANY_NAME} products, services, and solutions available on lxme.in.'
 COMPANY_BACKSTORY = (
     f'You are a knowledgeable specialist in {COMPANY_NAME}\'s offerings. '
     f'You provide detailed information about their products, services, '
-    f'and solutions available on axi.com, including any innovations and key features.'
+    f'and solutions available on lxme.in, including any innovations and key features.'
 )
 
 
@@ -88,7 +88,7 @@ centralized_task = Task(
         memory=True,
         backstory=(
             f'You are an intelligent bot specializing in {COMPANY_NAME} information. You provide detailed responses '
-            f'about {COMPANY_NAME}\'s trading platforms, learning platform.'
+            f'about {COMPANY_NAME}\'s trading platforms, financial instruments, account types, and market analysis tools. '
             f'You only respond to queries related to {COMPANY_NAME}.'
         ),
         tools=[search_tool],
@@ -115,32 +115,32 @@ body {
 
 /* Change the color of the main title */
 h1 {
-    color: #dc3545;
+    color: #bf1f61;
 }
 
 /* Style the chat messages */
 .chat-message.user {
     background-color: #ffcccb;
-    color: #dc3545;
-    border: 2px solid #dc3545;
+    color: #bf1f61;
+    border: 2px solid #bf1f61;
 }
 
 .chat-message.assistant {
     background-color: #ffffcc;
-    color: #dc3545;
-    border: 2px solid #dc3545;
+    color: #bf1f61;
+    border: 2px solid #bf1f61;
 }
 
 /* Style the input box at the bottom */
 .stTextInput > div {
     background-color: #ffcccb;
     border-radius: 5px;
-    color: #dc3545;
+    color: #bf1f61;
 }
 
 /* Style the buttons */
 button {
-    background-color: #dc3545;
+    background-color: #bf1f61;
     color: #fff;
    
     border: none;
@@ -148,26 +148,22 @@ button {
 }
 
 .st-emotion-cache-1ghhuty{
-background-color: #dc3545;
+background-color: #bf1f61;
 }
 
 .st-emotion-cache-bho8sy{
-background-color: #ffc107;
+background-color: black;
 }
 /* Style the spinner */
 .stSpinner > div {
-    border-top-color: #dc3545;
+    border-top-color: #bf1f61;
 }
 
 /* Style the download button */
 .stDownloadButton {
-    background-color: #dc3545;
+    background-color: #bf1f61;
     color: #fff;
     border-radius: 5px;
-}
-
-.st-emotion-cache-1dp5vir{
-background-image: linear-gradient(90deg, rgb(255, 75, 75), rgb(255, 253, 128));
 }
 
 .black-text {
@@ -183,13 +179,9 @@ st.markdown(custom_css, unsafe_allow_html=True)
 
 # Streamlit UI
 st.markdown("""
-          
-          
-    <img class="logo-footer" style=margin-right:3%; src="https://d2tpnh780x5es.cloudfront.net/rebrand-prod/onla2r0j/logo-red2.svg" alt="AxiCorp">        
-    <h4 style="color:#ffc107;">
-           Customer Support
-    </h4>
-  
+    <h1 style="color:black;">
+        L<span style="color:#bf1f61;">X</span>ME Customer Support
+    </h1>
 """, unsafe_allow_html=True)
 st.write("<style>div.block-container{padding-top:2rem;}</style>", unsafe_allow_html=True)
 
